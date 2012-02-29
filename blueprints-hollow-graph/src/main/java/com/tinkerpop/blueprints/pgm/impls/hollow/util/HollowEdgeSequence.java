a3fc4bac30e726e9ba57c1793be0d666d99c593e
Daniel Margo <dmargo@zod.(none)>
2012-02-28 19:07:56 -0500

    Fixed hollow iters a little bit.

--- blueprints-hollow-graph/src/main/java/com/tinkerpop/blueprints/pgm/impls/hollow/util/HollowEdgeSequence.java
+++ blueprints-hollow-graph/src/main/java/com/tinkerpop/blueprints/pgm/impls/hollow/util/HollowEdgeSequence.java
@@ -6,17 +6,24 @@ import com.tinkerpop.blueprints.pgm.impls.hollow.HollowEdge;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 /**
  * @author Elaine Angelino (http://www.eecs.harvard.edu/~elaine)
  */
 public class HollowEdgeSequence implements Iterable<Edge>, Iterator<Edge> {
+	
+	private static Random rand = new Random();
 
     private HollowGraph graph = null;
     private int i = 0;
+    private int degree = 0;
     
     public HollowEdgeSequence(final HollowGraph graph) {
         this.graph = graph;
+        
+        int avgDegree = (graph.edgeCount + graph.vertexCount - 1) / graph.vertexCount;
+        this.degree = rand.nextInt(2 * avgDegree + 1);
     }
     
     public HollowEdgeSequence(final HollowGraph graph, final long vid, final boolean getOut) {
@@ -28,7 +35,7 @@ public class HollowEdgeSequence implements Iterable<Edge>, Iterator<Edge> {
     }
 	
 	public Edge next() {
-		if (i++ < this.graph.edgeCount / this.graph.vertexCount ) {
+		if (i++ < degree ) {
 			return new HollowEdge(this.graph, 0, 0, 0, "");
 		} else {
 			throw new NoSuchElementException();
@@ -36,11 +43,11 @@ public class HollowEdgeSequence implements Iterable<Edge>, Iterator<Edge> {
 	}
 
 	public boolean hasNext() {
-    	return i < this.graph.edgeCount / this.graph.vertexCount;
+    	return i < degree;
     }
 	
 	public void close() {
-		i = this.graph.edgeCount / this.graph.vertexCount;
+		i = degree;
 	}
 
     public void remove() { 
